31efc1bea3f32bef2c3f96461e2dc80a21655139
loay badwy <loaybadwy77@gmail.com>
2022-12-23 20:25:00 +0200

    setup globalization and localization

--- WATERWebsite/Program.cs
+++ WATERWebsite/Program.cs
@@ -1,5 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
+using Westwind.Globalization.AspnetCore;
 
+var builder = WebApplication.CreateBuilder(args);
+/**/
+// Standard ASP.NET Localization features are recommended
+// Make sure this is done FIRST!
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Properties";
+});
+
+
+// Replace StringLocalizers with Db Resource Implementation
+builder.Services.AddSingleton(typeof(IStringLocalizerFactory),
+                      typeof(DbResStringLocalizerFactory));
+builder.Services.AddSingleton(typeof(IHtmlLocalizerFactory),
+                      typeof(DbResHtmlLocalizerFactory));
+
+
+// Required: Enable Westwind.Globalization (opt parm is optional)
+// shown here with optional manual configuration code
+builder.Services.AddWestwindGlobalization();
+/**/
 // Add services to the container.
 builder.Services.AddRazorPages();
 
