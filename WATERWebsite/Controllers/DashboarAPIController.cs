9ad8d00ff97523f737bc296f38e67d9035763328
loay badwy <loaybadwy77@gmail.com>
2024-01-13 00:14:29 +0200

    add blog api

---
+++ WATERWebsite/Controllers/DashboarAPIController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterClassLibrary.Presistance;
+
+namespace WATERWebsite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboarAPIController : ControllerBase
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        public DashboarAPIController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost]
+        [Route("UploadToWebsite")]
+        public IActionResult UploadToWebsite([FromForm] IFormFile PhotoFile, [FromForm] string imageName)
+        {
+            var path = Path.Combine($"{_webHostEnvironment.WebRootPath}/resourses/Blog", imageName);
+
+            using var stream = System.IO.File.Create(path);
+            PhotoFile.CopyTo(stream);
+            return Ok("sss");
+        }
+    }
+}
